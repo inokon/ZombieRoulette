560e07d195ef01ec31f84fec6ffbde47a8a7150f
zmontesd <zmontesd@gmail.com>
2013-04-13 16:25:59 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZombieRoulette::Application.config.secret_token = 'd6daab71f55ef0d5c11345df7bd2b1113f397e189a3d68c0a9d85bee0b6964b4798ed3260f417c73c3ca2018db3f5d4111706a486a38091f0a9914d8289d4859'
